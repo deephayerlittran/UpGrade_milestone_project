88ec4ed4bdd1f5fb727302eb6d0f7fedf213f993
Karanveer Singh <karanveersingh7@rrc.ca>
2025-01-31 14:38:00 -0600

    Defined tests for Integrated API routes

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,5 @@ app.get("/health", (req, res) => {
   res.send("Server is healthy");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
-});
 
 export default app;
