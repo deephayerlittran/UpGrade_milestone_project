4ea96205fe496ec460c59a1ca339bf5c67969a08
Karanveer Singh <karanveersingh7@rrc.ca>
2025-01-31 14:26:18 -0600

    Defined all test cased for itemservices.test.ts

--- test/itemservices.test.ts
+++ test/itemservices.test.ts
@@ -4,5 +4,20 @@ describe('Item Services', () => {
     test('fetchAllItems should return an array of items', () => {
         const result = fetchAllItems();
         expect(result).toEqual(["Item 1", "Item 2"]);
-    })
-})
+    });
+
+    test('addItem should return "Item added"', () => {
+        const result = addItem('Item 3');
+        expect(result).toBe("Item added");
+    });
+
+    test('ModifyItem should return "ItemUpdated"', () =>{
+        const result = modifyItem(1, 'Modified Item');
+        expect(result).toBe("Item updated");
+    });
+
+    test('removeItem should return "Item deleted"', () => {
+        const result = removeItem(1);
+        expect(result).toBe("Item deleted");
+    });
+});
